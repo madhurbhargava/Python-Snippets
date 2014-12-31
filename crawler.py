342f352251828936b870ca4e4b0d7b8788a44848
Madhur Bhargava <madhurbhargava@VIGNETWKS-134.local>
2014-12-31 15:19:54 +0530

    First commit

---
+++ crawler.py
@@ -0,0 +1,14 @@
+#! Crawler
+
+import re, urllib
+
+textfile = file('d1.txt','wt')
+print "Enter the URL you wish to crawl.."
+print 'Usage  - "http://www.google.com" <-- With the double quotes'
+myurl = input("@> ")
+for i in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(myurl).read(), re.I):
+        print i  
+        for ee in re.findall('''href=["'](.[^"']+)["']''', urllib.urlopen(i).read(), re.I):
+                print ee
+                textfile.write(ee+'\n')
+textfile.close()
